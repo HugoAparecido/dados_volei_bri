3877ca375b3d280a1b1ed48fb177bed27ca7af0b
HugoAparecido <120059026+HugoAparecido@users.noreply.github.com>
2023-11-15 22:59:05 -0300

    15/11/2023

--- js/inserir_informacoes.js
+++ js/inserir_informacoes.js
@@ -8,13 +8,33 @@ const app = initializeApp(appSettings)
 const database = getDatabase(app)
 const jogadorInDB = ref(database, "jogador")
 // transformando os campos em variáveis
-const selecionar_jogador = document.getElementById("nome")
+const selecionarJogador = document.getElementById("nome")
+const salvar = document.getElementById("salvar")
+const saque = document.getElementById("saque")
+const tipoSaque = document.getElementById("tipo_saque")
+const ace = document.getElementById("ace")
 // Ações Botões
 onValue(jogadorInDB, function (snapshot) {
     let jogadoresArray = Object.values(snapshot.val())
     for (let i = 0; i < jogadoresArray.length; i++) {
         let jogadorAtual = jogadoresArray[i]
-        let jogadorAtualObject = JSON.parse(jogadorAtual)
-        selecionar_jogador.innerHTML += `<option value="${jogadorAtualObject.nome}">${jogadorAtualObject.nome}</option>`
+        selecionarJogador.innerHTML += `<option value="${jogadorAtual.nome}">${jogadorAtual.nome}</option>`
+    }
+})
+salvar.addEventListener("click", function () {
+    var idJogador = ""
+    var jogadorSelecionado = ""
+    onValue(jogadorInDB, function (snapshot) {
+        let jogadoresArray = Object.entries(snapshot.val())
+        for (let i = 0; i < jogadoresArray.length; i++) {
+            let jogadorAtual = jogadoresArray[i]
+            if (jogadorAtual[1].nome === selecionarJogador.value) {
+                idJogador = jogadorAtual[0]
+                jogadorSelecionado = jogadorAtual[1]
+            }
+        }
+    })
+    if (document.querySelector('input[name="ace"]:checked').value === "sim") {
+        push(ref(database, `jogador/${idJogador}/${document.querySelector('input[name="saque"]:checked').value}/ace/${tipoSaque.value}`), (jogadorSelecionado.saque_dentro.ace[tipoSaque.value] += 1))
     }
 })
