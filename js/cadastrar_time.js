5df073c1d2b53261bf1fc9096af709a3bd42a4d6
HugoAparecido <120059026+HugoAparecido@users.noreply.github.com>
2023-11-20 14:04:26 -0300

    20/11/2023

--- js/cadastrar_time.js
+++ js/cadastrar_time.js
@@ -10,7 +10,10 @@ const jogadorInDB = ref(database, "jogador")
 const timeInDB = ref(database, "time")
 // transformando campos em variáveis
 const nomeTime = document.getElementById("nome_time")
-const adicionarJogadores = document.getElementById("adicionar_jogadores")
+const sexotime = document.getElementById("sexo_time")
+const botaoCadastrarTime = document.getElementById("cadastrar_time")
+const exibicaoTimes = document.getElementById("times_cadastrados")
+const mostrarTimes = document.getElementById("mostrar_times")
 // colocando valores nos selects
 const idJogador = [document.getElementById("jogador1"), document.getElementById("jogador2"), document.getElementById("jogador3"), document.getElementById("jogador4"), document.getElementById("jogador5"), document.getElementById("jogador6")]
 for (let i = 0; i < 6; i++) {
@@ -22,3 +25,27 @@ for (let i = 0; i < 6; i++) {
         }
     })
 }
+// cadastrar o time
+botaoCadastrarTime.addEventListener("click", function () {
+    let valorCadastar = {
+        nome: nomeTime.value,
+        sexo: sexotime.value,
+        levantador: idJogador[0].value,
+        oposto: idJogador[1].value,
+        central: idJogador[2].value,
+        ponteiro_passador1: idJogador[3].value,
+        ponteiro_passador2: idJogador[4].value,
+        libero: idJogador[5].value,
+    }
+    push(timeInDB, valorCadastar)
+    alert("Time Cadastrado com Sucesso")
+})
+mostrarTimes.addEventListener("click", function () {
+    onValue(timeInDB, function (snapshot) {
+        let timesArray = Object.values(snapshot.val())
+        for (let i = 0; i < timesArray.length; i++) {
+            let timeAtual = timesArray[i]
+            exibicaoTimes.innerHTML += `<tr><td>${timeAtual.nome}</td><td>${timeAtual.sexo}</td><td>${timeAtual.levantador}</td><td>${timeAtual.oposto}</td><td>${timeAtual.central}</td><td>${timeAtual.ponteiro_passador1}</td><td>${timeAtual.ponteiro_passador2}</td><td>${timeAtual.libero}</td></tr>`
+        }
+    })
+})
