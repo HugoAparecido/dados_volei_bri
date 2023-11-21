a9cc7674863bc465cb02791a917ca8ae9f0f602e
HugoAparecido <120059026+HugoAparecido@users.noreply.github.com>
2023-11-21 20:09:11 -0300

    Update inserir_informacoes.js

--- js/inserir_informacoes.js
+++ js/inserir_informacoes.js
@@ -46,6 +46,30 @@ onValue(jogadorInDB, function (snapshot) {
         }
     }
 })
+selecionarTime.addEventListener("change", function () {
+    onValue(jogadorInDB, function (snapshot) {
+        var idTime = {}
+        var timeSelecionado = {}
+        onValue(timeInDB, function (snapshot) {
+            let timesArray = Object.entries(snapshot.val())
+            for (let i = 0; i < timesArray.length; i++) {
+                let timeAtual = timesArray[i]
+                if (timeAtual[1].nome === selecionarTime.value) {
+                    idTime = timeAtual[0]
+                    timeSelecionado = timeAtual[1]
+                }
+            }
+        })
+        selecionarJogador.innerHTML = "";
+        let jogadoresArray = Object.values(snapshot.val())
+        for (let i = 0; i < jogadoresArray.length; i++) {
+            let jogadorAtual = jogadoresArray[i]
+            if (Object.values(jogadorAtual).includes(timeSelecionado.levantador) || Object.values(jogadorAtual).includes(timeSelecionado.oposto) || Object.values(jogadorAtual).includes(timeSelecionado.central) || Object.values(jogadorAtual).includes(timeSelecionado.ponteiro_passador1) || Object.values(jogadorAtual).includes(timeSelecionado.ponteiro_passador2) || Object.values(jogadorAtual).includes(timeSelecionado.libero)) {
+                selecionarJogador.innerHTML += `<option value="${jogadorAtual.nome}">${jogadorAtual.nome}</option>`
+            }
+        }
+    })
+})
 // cadastrar saque
 salvar.addEventListener("click", function () {
     var idJogador = {}
