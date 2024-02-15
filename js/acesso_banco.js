aa61581e3318873a47ca56c9233811e40f6322b9
HugoAparecido <120059026+HugoAparecido@users.noreply.github.com>
2024-02-15 19:16:24 -0300

    Adição de comentários para acesso e cadastrar

--- js/acesso_banco.js
+++ js/acesso_banco.js
@@ -1,11 +1,15 @@
 // Import the functions you need from the SDKs you need
+// importando a inicialização do firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+// importando o adquirimento de usuario
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+// importando a inicialização para o banco
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// configuração para utilizar o banco de dados
 const firebaseConfig = {
     apiKey: "AIzaSyDYZq7RYpL4yJRxNzuijG0FDBVipoMZH3g",
     authDomain: "volei-birigui.firebaseapp.com",
@@ -17,5 +21,6 @@ const firebaseConfig = {
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+// exportando as duas constantes
 export const auth = getAuth();
 export const db = getFirestore(app);
