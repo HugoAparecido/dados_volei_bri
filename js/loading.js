6d4e5fdb07a0bd4a46c6acf48fd00d38b3f05ff8
HugoAparecido <120059026+HugoAparecido@users.noreply.github.com>
2024-02-17 10:27:46 -0300

    Update loading.js

--- js/loading.js
+++ js/loading.js
@@ -1,11 +1,13 @@
+// Criar uma div que ocupa a página toda exercendo a função de tela de carregamento
 export function ShowLoading() {
     const div = document.createElement("div")
     div.classList.add("loading", "centralize")
-    const label = document.createElement("span")
-    label.innerText = "Carregando..."
-    div.appendChild(label)
+    const span = document.createElement("span")
+    span.innerText = "Carregando..."
+    div.appendChild(span)
     document.body.appendChild(div)
 }
+// removendo a div de carregamento
 export function HideLoading() {
     const loadings = document.getElementsByClassName("loading");
     if (loadings.length) {
