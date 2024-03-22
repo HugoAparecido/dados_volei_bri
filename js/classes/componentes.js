d69fe424aeb9d8343f10950dafb804a542cfd2b1
hugoapga626@gmail.com <hugoapga626@gmail.com>
2024-03-22 09:19:40 -0300

    div

--- js/classes/componentes.js
+++ js/classes/componentes.js
@@ -1,22 +1,29 @@
-export class Componentes{
-  TabelaJogadores(id, nome, numero, posicao, sexo){
+export class Componentes {
+  TabelaJogadores(id, nome, numero, posicao, sexo) {
     return `<tr>
               <td>${id}</td>
               <td>${nome}</td>
               <td>${numero}</td>
               <td>${posicao}</td>
               <td>${sexo}</td>
-            </tr>`
+            </tr>`;
   }
-  SelectJogadores(id, numero , nome, posicao){
-    return `<option value="${id}">${numero == undefined ? " " : numero}: ${nome} (${posicao})</option>`;
+  SelectJogadores(id, numero, nome, posicao) {
+    return `<option value="${id}">${
+      numero == undefined ? " " : numero
+    }: ${nome} (${posicao})</option>`;
   }
-  DivJogador(posicao, numero, nome){
-    return `<div class="itemArrastavel jogador_dados" draggable="true">
-              <h3>${posicao}: ${numero == undefined ? " " : numero} ${nome}</h3>
-              <div class="insercao_individual">
-                <div></div>
-              </div>
-            </div>`
+  DivJogador(posicao, numero, nome) {
+    let divJogador = `<div class="itemArrastavel jogador_dados" draggable="true">
+                        <h3>${posicao}: ${
+      numero == undefined ? " " : numero
+    } ${nome}</h3>
+                        <div class="insercao_individual">`;
+    if (posicao != "Levantador")
+      divJogador += `<div class="passes">
+                        <span><strong>Pas: </strong></span>
+                    </div>`;
+    divJogador += `</div></div>`;
+    return;
   }
 }
